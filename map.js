3fd91502714702aff9a18797a682975b37a82762
GitGeddes <hellohead102@gmail.com>
2019-02-26 23:27:26 -0500

    Implemented dragging restrictions and zooming on mouse position

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
 var rows = 25;
 var cols = 43;
+var zDelta = 0.1; // zoom delta
 var dragging = false;
 var unlockedChunks = [];
 var potentialChunks = [];
@@ -9,13 +10,124 @@ var borderWidth = " 2px";
 $(document).ready(function () {
 	buildArray(rows, cols);
 
+	// Get the div holding the map images and grid of buttons
+	var imageDiv = document.getElementById("imgDiv");
+
+	// Reposition the map to be centered on Lumbridge
+	repositionMap(imageDiv);
+
 	// Allow dragging the map, and set a flag when dragging
 	$("#imgDiv").draggable({
-		drag: function () {
+		drag: function (event, ui) {
 			dragging = true;
+
+			var margin = 50;
+			// Restrict how far the user can drag the map
+			if (ui.position.left < -imageDiv.offsetWidth + window.innerWidth - margin) {
+				ui.position.left = -imageDiv.offsetWidth + window.innerWidth - margin;
+			}
+			if (ui.position.left > margin) {
+				ui.position.left = margin;
+			}
+			if (ui.position.top < -imageDiv.offsetHeight + window.innerHeight - margin) {
+				ui.position.top = -imageDiv.offsetHeight + window.innerHeight - margin;
+			}
+			if (ui.position.top > margin) {
+				ui.position.top = margin;
+			}
 		}
 	});
+
+	// Zoom on map
+	window.addEventListener('wheel', function(e) {
+		e.preventDefault();
+
+		var dir; // Direction of scroll
+		if (e.deltaY > 0) dir = -zDelta;
+		else dir = zDelta;
+		// zScale += dir;
+
+		// Set minimum and maximum zoom of map
+		var minWidth = 0.95 * window.innerWidth
+		var maxWidth = 5 * window.innerWidth
+		if (imageDiv.offsetWidth == minWidth && dir < 0) {
+			// Zooming out would do nothing
+			// zScale -= dir;
+			return;
+		}
+		else if (imageDiv.offsetWidth == maxWidth && dir > 0) {
+			// Zooming in would do nothing
+			// zScale -= dir;
+			return;
+		}
+		else if (imageDiv.offsetWidth * (1 + dir) <= minWidth) {
+			// zScale -= dir;
+			// Calculate the percent difference between the previous and new width
+			dir = (minWidth - imageDiv.offsetWidth) / imageDiv.offsetWidth;
+			imageDiv.style.width = minWidth + "px";
+		}
+		else if (imageDiv.offsetWidth * (1 + dir) >= maxWidth) {
+			// zScale -= dir;
+			// Calculate the percent difference between the previous and new width
+			dir = (maxWidth - imageDiv.offsetWidth) / imageDiv.offsetWidth;
+			imageDiv.style.width = maxWidth + "px";
+		}
+		else {
+			imageDiv.style.width = (imageDiv.offsetWidth * (1 + dir)) + "px";
+		}
+		
+		// Zoom on the mouse position
+		zoomOnMouse(e, dir);
+		// Fix the location of the map because it could go off-screen
+		fixMapEdges();
+	});
+
+	// Zoom on the mouse location
+	function zoomOnMouse(event, dir) {
+		var leftNumber = Number(imageDiv.style.left.slice(0, -2));
+		var topNumber = Number(imageDiv.style.top.slice(0, -2));
+		var currentMouseX = Math.round(event.clientX);
+		var currentMouseY = Math.round(event.clientY);
+
+		// As image zooms, shift top-left corner closer to or further from mouse position
+		var offsetX = (currentMouseX - leftNumber) * dir;
+		var offsetY = (currentMouseY - topNumber) * dir;
+
+		imageDiv.style.left = (leftNumber - offsetX) + "px";
+		imageDiv.style.top = (topNumber - offsetY) + "px";
+	}
+
+	function fixMapEdges() {
+		// Take the "px" off the end and cast from a String to a Number
+		var leftNumber = Number(imageDiv.style.left.slice(0, -2));
+		var topNumber = Number(imageDiv.style.top.slice(0, -2));
+		var rightEdge = leftNumber + imageDiv.offsetWidth;
+		var bottomEdge = topNumber + imageDiv.offsetHeight;
+
+		var margin = 50;
+		if (leftNumber > margin) {
+			imageDiv.style.left = margin + "px";
+		}
+		if (rightEdge < window.innerWidth - margin) {
+			imageDiv.style.left = (window.innerWidth - margin) - imageDiv.offsetWidth + "px";
+		}
+		if (topNumber > margin) {
+			imageDiv.style.top = margin + "px";
+		}
+		if (bottomEdge < window.innerHeight - margin) {
+			imageDiv.style.top = (window.innerHeight - margin) - imageDiv.offsetHeight + "px";
+		}
+	}
 });
+
+function repositionMap(imageDiv) {
+	// Chunk width is 96 pixels wide when the map is initially 4128 pixels wide
+	// Lumbridge is at chunk (32, 13), so position in the middle of that chunk
+	var lumbyX = 96 * 32.5;
+	var lumbyY = 96 * 13.5;
+	imageDiv.style.left = Math.round(-lumbyX + window.innerWidth / 2) + "px";
+	imageDiv.style.top = Math.round(-lumbyY + window.innerHeight / 2) + "px";
+}
 
 /**
  * Called from html, filters out drag clicks
